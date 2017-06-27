bda85cfd71791dfecba1d001c7484dd0b28ff949
Luke Deen Taylor <ldt@deentaylor.com>
2017-06-26 19:23:08 -0700

    Update .hyper.js

--- .hyper.js
+++ .hyper.js
@@ -1,58 +1,105 @@
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
 module.exports = {
   config: {
     // default font size in pixels for all tabs
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: '"SourceCodePro+Powerline+Awesome Regular", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
-    // terminal cursor background color (hex)
-    cursorColor: '#F81CE5',
+    // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+    cursorColor: '#cdd2e9',
 
-    windowSize: [890, 550],
+    // `BEAM` for |, `UNDERLINE` for _, `BLOCK` for █
+    cursorShape: 'BEAM',
+
+    // set to true for blinking cursor
+    cursorBlink: true,
 
     // color of the text
-    foregroundColor: '#fff',
+    foregroundColor: '#cdd2e9',
 
     // terminal background color
-    backgroundColor: '#000',
+    backgroundColor: 'rgba(41, 51, 65, 0)',
 
     // border color (window, tabs)
-    borderColor: '#333',
+    borderColor: 'rgba(0, 0, 0, 0)',
 
     // custom css to embed in the main window
-    css: '',
+    css: '.hyper_main { border: 0; }',
 
     // custom css to embed in the terminal window
-    termCSS: '',
+    termCSS: 'x-screen x-row { font-variant-ligatures: initial; }',
+
+    // set to `true` (without backticks) if you're using a Linux setup that doesn't show native menus
+    // default: `false` on Linux, `true` on Windows (ignored on macOS)
+    showHamburgerMenu: '',
+
+    // set to `false` if you want to hide the minimize, maximize and close buttons
+    // additionally, set to `'left'` if you want them on the left, like in Ubuntu
+    // default: `true` on windows and Linux (ignored on macOS)
+    showWindowControls: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
     padding: '12px 14px',
 
+    // the full list. if you're going to provide the full color palette,
+    // including the 6 x 6 color cubes and the grayscale map, just provide
+    // an array here instead of a color map object
+    colors: {
+      black: '#293341',
+      red: '#e17e85',
+      green: '#61ba86',
+      yellow: '#ffec8e',
+      blue: '#4cb2ff',
+      magenta: '#be86e3',
+      cyan: '#2dced0',
+      white: '#cdd2e9',
+      lightBlack: '#546386',
+      lightRed: '#e17e85',
+      lightGreen: '#61ba86',
+      lightYellow: '#ffec8e',
+      lightBlue: '#4cb2ff',
+      lightMagenta: '#be86e3',
+      lightCyan: '#2dced0',
+      lightWhite: '#cdd2e9'
+    },
+
+    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
+    // if left empty, your system's login shell will be used by default
+    // make sure to use a full path if the binary name doesn't work
+    // (e.g `C:\\Windows\\System32\\bash.exe` instead of just `bash.exe`)
+    // if you're using powershell, make sure to remove the `--login` below
     shell: '/usr/local/bin/zsh',
 
-    cursorShape: 'BEAM',
+    // for setting shell arguments (i.e. for using interactive shellArgs: ['-i'])
+    // by default ['--login'] will be used
+    shellArgs: ['--login'],
 
-    // some color overrides. see http://bit.ly/29k1iU2 for
-    // the full list
-    colors: [
-      '#000000',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#d0d0d0',
-      '#808080',
-      '#ff0000',
-      '#33ff00',
-      '#ffff00',
-      '#0066ff',
-      '#cc00ff',
-      '#00ffff',
-      '#ffffff'
-    ]
+    // for environment variables
+    env: {},
+
+    // set to false for no bell
+    bell: 'SOUND',
+
+    // if true, selected text will automatically be copied to the clipboard
+    copyOnSelect: false,
+
+    // if true, on right click selected text will be copied or pasted if no
+    // selection is present (true by default on Windows)
+    // quickEdit: true
+
+    // URL to custom bell
+    // bellSoundURL: 'http://example.com/bell.mp3',
+
+    // for advanced config flags please refer to https://hyper.is/#cfg
+
+    hyperTabs: {
+      trafficButtons: true,
+    },
   },
 
   // a list of plugins to fetch and install from npm
@@ -61,10 +108,18 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperterm-blink", "hyperlinks", "hyperterm-base16-ocean-saturated"],
+  plugins: [
+    "hyper-simple-vibrancy",
+    "hyperlinks",
+    "hypercwd",
+    "hyper-tabs-enhanced",
+    "hyperterm-title",
+    "hyperterm-alternatescroll",
+    "hyper-statusline"
+  ],
 
   // in development, you can create a directory under
-  // `~/.hyperterm_plugins/local/` and include it here
+  // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
   localPlugins: []
 };
